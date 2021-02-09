4c1fe2ae0c393dd1d3f01a7a047998108b32ef25
serene1004 <vcbxnzn@naver.com>
2021-02-09 18:39:27 +0900

    Update 2021-02-09 18:39

--- 02_08/02_08_javascript/js/script.js
+++ 02_08/02_08_javascript/js/script.js
@@ -4,30 +4,30 @@
 
 
 // 속성 값이 문자 또는 문자열은 반드시 '',"",`` 감싸준다
-var obj = {         // 객체 이름
-    irum:'김유신',  // 속성(Property) : 속성값(Value)
-    age:45,         // 객체안에 속성구분은 ,(콤마)로 함
-    mill:true,
-    result: function(){
+var obj = { // 객체 이름
+    irum: '김유신', // 속성(Property) : 속성값(Value)
+    age: 45, // 객체안에 속성구분은 ,(콤마)로 함
+    mill: true,
+    result: function () {
         // this 객체이름 obj 객체
         // 메소드 안에서는 객체 속성값 뒤에 = 을 사용한다.
-        
-        var cnt=0;
-            cnt=100;
-        console.log( this.irum='이순신', this.age =30, this.mill=false );
-        console.log('result 메소드' ,cnt);
+
+        var cnt = 0;
+        cnt = 100;
+        console.log(this.irum = '이순신', this.age = 30, this.mill = false);
+        console.log('result 메소드', cnt);
     },
 
-                        // 메소드(Method) 또는 리터럴 함수 또는 익명 함수(이름이 없는 함수)
-                        // 프로퍼티에 functuon(){} 이 존재하면 메소드가 된다.
+    // 메소드(Method) 또는 리터럴 함수 또는 익명 함수(이름이 없는 함수)
+    // 프로퍼티에 functuon(){} 이 존재하면 메소드가 된다.
 
-    answer: function(){
-        var cnt=0;
-            cnt=50;
-            console.log('answer 메소드' ,cnt);
+    answer: function () {
+        var cnt = 0;
+        cnt = 50;
+        console.log('answer 메소드', cnt);
     },
 
-    init:function(){
+    init: function () {
         this.result();
         this.answer();
     }
@@ -35,9 +35,9 @@ var obj = {         // 객체 이름
 
 };
 
-    obj.init();  // result 메소드 호출 실행
-    // console.log(obj.result());
-    // console.log(obj.answer());
+obj.init(); // result 메소드 호출 실행
+// console.log(obj.result());
+// console.log(obj.answer());
 
 
 
@@ -47,8 +47,53 @@ var obj = {         // 객체 이름
 // console.log( obj.age);   // 객체.나이 정수
 // console.log( obj.mill);  // 객체. 군복무 참/거짓
 
-// D:/국기과정_윤용준/교육산출물/02
 
+
+//성적처리 객체
+var sungjuk = {
+    init: function () {
+        this.tot();
+        this.avg();
+    },
+    irum: '커피커피',
+    kor: 90,
+    eng: 50,
+    mat: 70,
+    tot: function () {
+        console.log('이름', this.irum );
+        console.log('총점', this.kor+this.eng+this.mat );
+    },
+    avg: function () {
+        console.log('평균', (this.kor+this.eng+this.mat)/3 );
+    }
+
+}
+
+sungjuk.init();
+
+//  연습  //
+var amsa = {
+    init:function(){
+        this.total();
+        this.avg();
+    },
+    irum1:'용준',
+    irum2:'재승',
+    irum3:'성호',
+    irum4:'동우',
+
+    kor:50,
+    eng:70,
+    mat:60,
+    total:function(){
+        console.log('이름', this.irum1+this.irum2+this.irum3+this.irum4);
+    },
+    avg:function(){
+        console.log('평균', (this.kor+this.eng+this.mat)/3);
+    }
+}
+
+amsa.init();
 
 ////슬라이드1////
 
@@ -60,19 +105,19 @@ var $page = document.querySelector('.page')
 
 
 // 다음슬라이드 클릭시 -1200만큼 이동
-function nextSlideFn(){
-    cnt+=1;
-    cnt > 3 ? cnt=0 : cnt;
-    $slide.style = 'left:'+(-1200*cnt)+'px';
-    $page.innerHTML = cnt+1 +'/4';
+function nextSlideFn() {
+    cnt += 1;
+    cnt > 3 ? cnt = 0 : cnt;
+    $slide.style = 'left:' + (-1200 * cnt) + 'px';
+    $page.innerHTML = cnt + 1 + '/4';
 }
 
 
 
-function prevSlideFn(){
-    cnt-=1;
-    cnt < 0 ? cnt=3 : cnt;
-    $slide.style = 'left:'+(-1200*cnt)+'px';
+function prevSlideFn() {
+    cnt -= 1;
+    cnt < 0 ? cnt = 3 : cnt;
+    $slide.style = 'left:' + (-1200 * cnt) + 'px';
 }
 
 
@@ -81,7 +126,7 @@ function prevSlideFn(){
 ////// cnt > 2 ? cnt=2 : cnt; // 
 
 
-setInterval(nextSlideFn, 3000);  // 3초에 한번씩 자동으로 다음 슬라이드 호출 (1초가 1000)
+setInterval(nextSlideFn, 3000); // 3초에 한번씩 자동으로 다음 슬라이드 호출 (1초가 1000)
 //setTimeout(nextSlideFn, 5000);  // 5초후에 실행하고 끝 (타임아웃)
 
 
@@ -93,16 +138,16 @@ setInterval(nextSlideFn, 3000);  // 3초에 한번씩 자동으로 다음 슬라
 var cnt2 = 0;
 var $slide2 = document.querySelector('#section2 .left-box .slide-wrap')
 
-function nextSlideFn2(){
-    cnt2+=1;
-    cnt2 > 3 ? cnt2=0 : cnt2;
-    $slide2.style = 'left:'+(-400*cnt2)+'px';
+function nextSlideFn2() {
+    cnt2 += 1;
+    cnt2 > 3 ? cnt2 = 0 : cnt2;
+    $slide2.style = 'left:' + (-400 * cnt2) + 'px';
 }
 
-function prevSlideFn2(){
-    cnt2-=1;
-    cnt2 < 0 ? cnt2=3 : cnt2;
-    $slide2.style = 'left:'+(-400*cnt2)+'px';
+function prevSlideFn2() {
+    cnt2 -= 1;
+    cnt2 < 0 ? cnt2 = 3 : cnt2;
+    $slide2.style = 'left:' + (-400 * cnt2) + 'px';
 }
 
 
@@ -110,16 +155,16 @@ function prevSlideFn2(){
 var cnt3 = 0;
 var $slide3 = document.querySelector('#section2 .center-box .slide-wrap')
 
-function downSlideFn(){
-    cnt3+=1;
-    cnt3 > 3 ? cnt3=0 : cnt3;
-    $slide3.style = 'top:'+(-200*cnt3)+'px';
+function downSlideFn() {
+    cnt3 += 1;
+    cnt3 > 3 ? cnt3 = 0 : cnt3;
+    $slide3.style = 'top:' + (-200 * cnt3) + 'px';
 }
 
-function upSlideFn(){
-    cnt3-=1;
-    cnt3 < 0 ? cnt3=3 : cnt3;
-    $slide3.style = 'top:'+(-200*cnt3)+'px';
+function upSlideFn() {
+    cnt3 -= 1;
+    cnt3 < 0 ? cnt3 = 3 : cnt3;
+    $slide3.style = 'top:' + (-200 * cnt3) + 'px';
 }
 
 
@@ -127,14 +172,14 @@ function upSlideFn(){
 var cnt4 = 0;
 var $slide4 = document.querySelector('#section2 .right-box .slide-wrap')
 
-function nextSlideFn4(){
-    cnt4+=1;
-    cnt4 > 3 ? cnt4=0 : cnt4;
-    $slide4.style = 'left:'+(-400*cnt4)+'px';
+function nextSlideFn4() {
+    cnt4 += 1;
+    cnt4 > 3 ? cnt4 = 0 : cnt4;
+    $slide4.style = 'left:' + (-400 * cnt4) + 'px';
 }
 
-function prevSlideFn4(){
-    cnt4-=1;
-    cnt4 < 0 ? cnt4=3 : cnt4;
-    $slide4.style = 'left:'+(-400*cnt4)+'px';
+function prevSlideFn4() {
+    cnt4 -= 1;
+    cnt4 < 0 ? cnt4 = 3 : cnt4;
+    $slide4.style = 'left:' + (-400 * cnt4) + 'px';
 }
