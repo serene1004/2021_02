3e2981b9490911d7e11690c0978aafd2fc3a0d4b
serene1004 <vcbxnzn@naver.com>
2021-02-19 18:42:51 +0900

    Update 2021-02-19

--- 02_10/02_10_for연습/js/nav.js
+++ 02_10/02_10_for연습/js/nav.js
@@ -12,6 +12,28 @@
 // window.onload = function () {}
 
 
+
+////jQuery 사용법////
+// $(function(){
+//     alert('제이쿼리실행');
+// });
+
+// jQuery(function(){
+//     alert('제이쿼리실행!');
+// });
+
+;(function($){     ////             <<<<<<<<이걸로 사용함 위에 2개는 사용ㄴㄴㄴㄴ
+    
+    $('.main-btn').on({
+        click:function(){
+            alert();
+        }
+    })
+
+})(jQuery);   //IIFE(즉시실행표현식)
+
+/////////////////////////////////////////////////////
+
 var nav = {
     init: function () {
         this.navFn();
